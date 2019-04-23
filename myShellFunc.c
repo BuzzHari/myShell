b4d4ae4fb82cbef9a6fc2c4f2eb2f8f70c4b5834
BuzzHari <buzz.hari@gmail.com>
2019-04-23 08:49:36 +0530

    updated

--- myShellFunc.c
+++ myShellFunc.c
@@ -4,22 +4,22 @@
 #include<string.h>
 
 void initializeSimpleCommand(){
-    SIMPLE_COMMAND.arguments=(char**)malloc(sizeof(char*)*TOK_BUFSIZE);
-    if(!SIMPLE_COMMAND.arguments){
+    command.arguments=(char**)malloc(sizeof(char*)*TOK_BUFSIZE);
+    if(!command.arguments){
         printf("Malloc error\n");
         exit(EXIT_FAILURE);
     }
     
-    SIMPLE_COMMAND.numberOfArguments = 0;
-    SIMPLE_COMMAND.bufsize = TOK_BUFSIZE;
+    command.numberOfArguments = 0;
+    command.bufsize = TOK_BUFSIZE;
 }
 
 void insertArguments(char *args){
      
     //to take care of overflow.
-    if(SIMPLE_COMMAND.numberOfArguments >= SIMPLE_COMMAND.bufsize){
-        SIMPLE_COMMAND.bufsize+=SIMPLE_COMMAND.bufsize;
-        SIMPLE_COMMAND.arguments = realloc(SIMPLE_COMMAND.arguments, SIMPLE_COMMAND.bufsize*sizeof(char*));
+    if(command.numberOfArguments >= command.bufsize){
+        command.bufsize+=command.bufsize;
+        command.arguments = realloc(command.arguments, command.bufsize*sizeof(char*));
     }
     
     /*If the args from the parser is not null, append it in the list
@@ -27,14 +27,14 @@ void insertArguments(char *args){
      * A null, means end of the command so, if null, insert NULL in the end of the
      * args list.*/
     if(args!=NULL)
-        SIMPLE_COMMAND.arguments[(SIMPLE_COMMAND.numberOfArguments)++] = strdup(args);
+        command.arguments[(command.numberOfArguments)++] = strdup(args);
     else 
-        SIMPLE_COMMAND.arguments[(SIMPLE_COMMAND.numberOfArguments)++] = NULL;
+        command.arguments[(command.numberOfArguments)++] = NULL;
 }
 
 void printCurrentCommand(){
-    for(int i=0; SIMPLE_COMMAND.arguments[i] != NULL; i++)
-        printf("%s ", SIMPLE_COMMAND.arguments[i]);
+    for(int i=0; command.arguments[i] != NULL; i++)
+        printf("%s ", command.arguments[i]);
     printf("\n");
 }
 
